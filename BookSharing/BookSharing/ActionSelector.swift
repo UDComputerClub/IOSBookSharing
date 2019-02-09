b9ffbaf622840a698850b50dbd47102a4b551e4f
mbolot <mbolot@Pauls-Mac-mini.local>
2019-02-09 08:45:13 -0800

    Push for basic interfaces

---
+++ BookSharing/BookSharing/ActionSelector.swift
@@ -0,0 +1,13 @@
+//
+//  ActionSelector.swift
+//  BookSharing
+//
+//  Created by mbolot on 2/9/19.
+//  Copyright © 2019 mbolot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ActionSelector : UISegmentedControl{
+    
+}
