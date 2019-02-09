a985545f9d27ba048285f3c3ab614044ae00d4ff
mbolot <mbolot@Pauls-Mac-mini.local>
2019-02-09 08:16:01 -0800

    Initialization of the project

---
+++ BookSharing/BookSharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookSharing
+//
+//  Created by mbolot on 2/9/19.
+//  Copyright © 2019 mbolot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
